fcc2053f91ae0f2a0cd427be7dd98a2c1768cde8
Sumedh Indulkar <cscoder2002@gmail.com>
2021-01-25 18:53:57 +0530

    Updates

---
+++ precache-manifest.bfa88b66fc0925e15c7608ea91821c64.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d55be5fac1341215910290cfe13ee859",
+    "url": "/React-web1/index.html"
+  },
+  {
+    "revision": "870e11206e54ff4a62f1",
+    "url": "/React-web1/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "87734e6e607b62eab4d6",
+    "url": "/React-web1/static/css/main.95197654.chunk.css"
+  },
+  {
+    "revision": "870e11206e54ff4a62f1",
+    "url": "/React-web1/static/js/2.337e6e28.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/React-web1/static/js/2.337e6e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87734e6e607b62eab4d6",
+    "url": "/React-web1/static/js/main.4efd40a0.chunk.js"
+  },
+  {
+    "revision": "eff185bb2a71cd3388cf",
+    "url": "/React-web1/static/js/runtime-main.76ecf19e.js"
+  },
+  {
+    "revision": "93c3292dd023a82482496f87a1193a5f",
+    "url": "/React-web1/static/media/rocket.93c3292d.png"
+  },
+  {
+    "revision": "73939b13755c02079c42226c4243a589",
+    "url": "/React-web1/static/media/work.73939b13.png"
+  },
+  {
+    "revision": "bf0fb362abca26e82593c84f523620d4",
+    "url": "/React-web1/static/media/work.bf0fb362.jpg"
+  }
+]);
